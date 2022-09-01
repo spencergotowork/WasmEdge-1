f208c74de32204baa216b29c3ca6286bc7e7f1a0
hydai <hydai@users.noreply.github.com>
2022-09-01 04:26:30 +0000

    deploy: 27d10492c5d95505d90ae9b316510657578be5e3

---
+++ source-files.js
@@ -0,0 +1,6 @@
+var sourcesIndex = JSON.parse('{\
+"wasmedge_sdk":["",[["externals",[],["function.rs","global.rs","memory.rs","mod.rs","table.rs"]]],["compiler.rs","config.rs","executor.rs","import.rs","instance.rs","io.rs","lib.rs","module.rs","statistics.rs","store.rs","types.rs","utils.rs","vm.rs","wasi.rs"]],\
+"wasmedge_sys":["",[["instance",[],["function.rs","global.rs","memory.rs","mod.rs","module.rs","table.rs"]]],["ast_module.rs","async.rs","compiler.rs","config.rs","executor.rs","frame.rs","io.rs","lib.rs","loader.rs","statistics.rs","store.rs","types.rs","utils.rs","validator.rs","vm.rs"]],\
+"wasmedge_types":["",[],["error.rs","lib.rs"]]\
+}');
+createSourceSidebar();
